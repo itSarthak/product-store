c9ffb946a28ae7513d562c0d6ff561bb5c54b542
itSarthak <sarthakt3006@gmail.com>
2025-05-27 22:52:47 +0530

    fix: getting deployment ready

--- backend/src/main/java/com/veldtclix/productStore/ProductStoreApplication.java
+++ backend/src/main/java/com/veldtclix/productStore/ProductStoreApplication.java
@@ -2,13 +2,11 @@ package com.veldtclix.productStore;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import io.github.cdimascio.dotenv.Dotenv;
+
 @SpringBootApplication
 public class ProductStoreApplication {
 
 	public static void main(String[] args) {
-		Dotenv dotenv = Dotenv.load();
-		System.setProperty("DATABASE_URL", dotenv.get("DATABASE_URL"));
 		SpringApplication.run(ProductStoreApplication.class, args);
 	}
 
