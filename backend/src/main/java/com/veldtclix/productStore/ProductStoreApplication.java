e3f40b8efc6f01e7b277f271eab685064a0e457e
itSarthak <sarthakt3006@gmail.com>
2025-05-27 23:11:56 +0530

    fix: added logger to check mongo uri

--- backend/src/main/java/com/veldtclix/productStore/ProductStoreApplication.java
+++ backend/src/main/java/com/veldtclix/productStore/ProductStoreApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class ProductStoreApplication {
 
 	public static void main(String[] args) {
+		System.out.println("MongoDB URI: " + System.getenv("DATABASE_URL"));
 		SpringApplication.run(ProductStoreApplication.class, args);
 	}
 
